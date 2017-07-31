221d99abd4cf0589312b2ad989a824a28a53c44f
Guillermo Sampallo <guille@gmail.com>
2017-07-31 18:57:25 -0300

    Primera version tanto del programa para arduino (NodeMCU 1.0 en realidad) y una app basica para detectar los datos que envia.
    Signed-off-by: Guillermo Sampallo <guille@gmail.com>

---
+++ app/Socketest/src/Socketest.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Socketest {
+
+    public static void main(String[] args) throws IOException {
+
+    	Socketest sockettest = new Socketest();
+
+    }
+
+    public Socketest() throws IOException {
+    	ServerSocket serverSocket = new ServerSocket(4444);
+        try {
+
+        	while(true) {
+        		Servicio servicio = new Servicio(serverSocket.accept());
+        		//System.out.println("nuevo cliente");
+        		servicio.start();
+        	}
+
+
+
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(1);
+        }
+
+
+
+
+        serverSocket.close();
+    }
+
+
+    public class Servicio extends Thread {
+    	Socket socket;
+    	public Servicio(Socket socket) {
+    		this.socket = socket;
+    	}
+
+    	public void run() {
+    		try {
+    		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    		String inputLine;
+            //int i = 0;
+            while ((inputLine = in.readLine()) != null) {
+            	System.out.println(inputLine);
+            	//i++;
+            }
+            //out.close();
+            in.close();
+            socket.close();
+    		} catch (IOException e) {
+    			System.err.println(e.getMessage());
+    		}
+    	}
+    }
+}
---
+++ arduino/sensor_bici.ino
@@ -0,0 +1,97 @@
+#include <ESP8266WiFi.h>
+//Necesarios para la libreria de wifimanager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+/*
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+*/
+const char* servidor = "192.168.1.106";
+int puerto = 4444;
+
+int pinBike = D6;
+int pinLed = D7;
+
+int vueltas = 0;
+int vueltasAcum = 0;
+
+const char* ssid = "linksys";
+const char* password = "perromateo";
+
+void setup() {
+
+  Serial.begin(9600);
+  /*
+  lcd.begin();
+  lcd.backlight();
+  lcd.print("iniciando...");
+  */
+  pinMode(pinBike, INPUT);
+  attachInterrupt(digitalPinToInterrupt(pinBike), cuentaVueltas, RISING);
+  pinMode(pinLed, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+
+    //lcd.clear();
+    Serial.println(WiFi.localIP());
+    
+  }
+  
+  //lcd.clear();
+}
+
+
+unsigned long currentTime;
+float frequency;
+WiFiClient client;
+void loop() {
+
+  currentTime = millis();
+  frequency = getFrecuencia();
+
+  Serial.println(frequency);
+
+  /*
+  lcd.setCursor(0, 0);
+  lcd.print(frequency);
+  */
+  if (client.connect(servidor, puerto)) {
+    client.print(frequency);
+    client.print("\r\n");
+  }
+  //client.stop();
+
+}
+
+void brillo() {
+    digitalWrite(pinLed, HIGH);
+    delay(300);
+    digitalWrite(pinLed, LOW);
+    delay(300);
+    digitalWrite(pinLed, HIGH);
+    delay(300);
+    digitalWrite(pinLed, LOW);
+}
+
+
+
+void cuentaVueltas() {
+  vueltas++;
+}
+int measureInterval = 1000;
+
+float getFrecuencia() {
+  vueltas = 0;
+
+  interrupts();
+  delay(measureInterval);
+  noInterrupts();
+
+  return (float)vueltas * 1000 / measureInterval;
+}
+
